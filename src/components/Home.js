5659e0a8a58eeb83c4eb1ec33e58fa51360f324b
shradhamenon <shradhamenon@hotmail.com>
2020-12-24 22:32:09 -0500

    App setup + Home page

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Home.css'
+
+class Home extends Component {
+  render() {
+    return(
+    <div class = 'content'>
+      <div class='header'>
+        <span>The Shoppies</span>
+      </div>
+      <div>
+        <span class = 'enter'>Enter</span>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default Home;
